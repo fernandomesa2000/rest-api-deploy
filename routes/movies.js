2e8291f0e6f3a5f5d82b3958607efde42c5807fe
Fernando Bedoya <juanfer30082000@gmail.com>
2025-05-22 10:10:04 -0500

    MVC

---
+++ routes/movies.js
@@ -0,0 +1,13 @@
+import { Router } from "express"
+
+import { MovieController } from "../Controller/movies.js";
+
+export const moviesRouter = Router()
+
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.post('/', MovieController.create)
+
+moviesRouter.get('/:id', MovieController.getById)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/:id', MovieController.update)
+
